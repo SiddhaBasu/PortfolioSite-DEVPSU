aa6b70e862675e22f372c664dfe0b626e66613c4
SiddhaBasu <basu.siddha@gmail.com>
2024-02-06 16:05:23 -0500

    Final before devpsu meeting

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-const http = require("node:http");
-const fs = require("fs"); // file system
-
+//const http = require("node:http");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const projects = [
+    {
+        title: 'Portfolio Website',
+        description: 'Portfolio website that displays basic professional information such as a bio, resume, contact info, and also showcases projects. Developed with Node.js',
+        image: 'node-js-736399_1280.png',
+        link: 'https://github.com/SiddhaBasu/PortfolioSite-DEVPSU'
+    },
+    {
+        title: 'Project 2 Title',
+        description: 'Project 2 Description',
+        image: 'path/to/project2_image.jpg',
+        link: 'project2_link'
+    },
+    // Add more projects as needed
+];
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.ejs', { projects });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on http://${hostname}:${port}`);
+});
+
+/*
 const server = http.createServer((req, res) => {
 
     let filePath = __dirname + '/public/index.html';
@@ -19,6 +54,8 @@ const server = http.createServer((req, res) => {
             break;
     }
 
+    res.render
+
     fs.readFile(filePath, (err, data) => {
         res.statusCode = 200; // ok
         res.setHeader('Content-Type', 'text/html');
@@ -27,6 +64,8 @@ const server = http.createServer((req, res) => {
 })
 
 
+
 server.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}`);
 });
+*/
